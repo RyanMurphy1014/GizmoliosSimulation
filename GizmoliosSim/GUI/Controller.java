1ba7f9558f1ffa53f507313bf5804f937e16dc29
RyanMurphy1014 <ryan.murphy611@gmail.com>
2019-04-22 00:01:54 -0400

    Began work on modeling the UI.
    It doesn't look pretty yet but this is the basics.

--- GizmoliosSim/GUI/Controller.java
+++ GizmoliosSim/GUI/Controller.java
@@ -1,27 +1,26 @@
 package GUI;
 
 
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
+import javafx.scene.control.ChoiceBox;
+import javafx.scene.control.ComboBox;
 
 import java.net.URL;
 import java.util.ResourceBundle;
 
 public class Controller implements Initializable {
+	
+	public ComboBox<String> algorithms;
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
-        System.out.println("View is now loaded!");
-    }
+    	algorithms.getItems().addAll("Highest Penalty First", "Smart Penalty");
     
-    public Button button;
-    
-    
-    
-    public void click(){
-    	System.out.println("The button has been clicked.");
-    	
     }
     
     
+
 }
