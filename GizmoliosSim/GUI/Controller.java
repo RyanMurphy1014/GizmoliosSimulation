751831427151a9a96e8a3eb4d60f0c6eaa5a8f67
RyanMurphy1014 <ryan.murphy611@gmail.com>
2019-04-22 11:55:11 -0400

    Created a speed slider
    This will adjust how many hours per second the simulation runs at. The
    exit button also works now.

--- GizmoliosSim/GUI/Controller.java
+++ GizmoliosSim/GUI/Controller.java
@@ -1,12 +1,15 @@
 package GUI;
 
 
+import javafx.beans.binding.Bindings;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
 import javafx.scene.control.ChoiceBox;
 import javafx.scene.control.ComboBox;
+import javafx.scene.control.Label;
+import javafx.scene.control.Slider;
 
 import java.net.URL;
 import java.util.ResourceBundle;
@@ -15,12 +18,40 @@ public class Controller implements Initializable {
 	
 	public ComboBox<String> algorithms;
 
+	public Button exit;
+	
+	public Slider speedControl;
+	public Label hourPerSecond;
+	
+	public void exitClick() {
+		System.exit(0);
+	}
+	
     @Override
     public void initialize(URL location, ResourceBundle resources) {
     	algorithms.getItems().addAll("Highest Penalty First", "Smart Penalty");
+    	
+    	speedControl.setMin(0);
+    	speedControl.setMax(12);
+    	speedControl.setValue(6);
+    	speedControl.setShowTickLabels(true);
+    	speedControl.setMajorTickUnit(1);
+    	speedControl.setBlockIncrement(1);
+    	
+    	
+    	hourPerSecond.textProperty().bind(
+                Bindings.format(
+                    "%.0f hours per second",
+                    speedControl.valueProperty()
+                )
+            );
     
     }
     
+    public void updateSpeed() {
+    	hourPerSecond.setText(Double.toString(speedControl.getValue()));
+    	System.out.println();
+    }
     
 
 }
--- GizmoliosSim/GUI/GizmoliosSimLauncher.java
+++ GizmoliosSim/GUI/GizmoliosSimLauncher.java
@@ -124,8 +124,8 @@ public class GizmoliosSimLauncher extends Application{
 	@Override
 	  public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
-        primaryStage.setTitle("Hello World");
-        primaryStage.setScene(new Scene(root, 1280, 720));
+        primaryStage.setTitle("Gizmolio Simulation");
+        primaryStage.setScene(new Scene(root, 900, 600));
         primaryStage.show();
     }
 
